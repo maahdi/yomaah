6f670a894060413b14fdd58ba8c0589d44b84b3f
yoshi <josien-lionel@hotmail.fr>
2014-03-14 00:12:31 +0100

    ajout route post-log

--- src/Yomaah/structureBundle/Resources/config/routing.php
+++ src/Yomaah/structureBundle/Resources/config/routing.php
@@ -5,4 +5,7 @@ use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
 
+$collection->add('post_log',new Route('/post_log', array(
+    '_controller' => 'yomaahBundle:Main:postLogout',)
+));
 return $collection;
